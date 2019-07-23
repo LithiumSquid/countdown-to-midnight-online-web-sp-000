4ad82aba7858c05cd6428283b15c752a3d852d5e
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-07-23 20:50:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,15 @@
 #write your code here
-
+number = 10
 def countdown(number)
-  number = 10
   while number > 10
     puts "#{number} SECOND(S)"
     number -= 1
   until number == 0
     puts "HAPPY NEW YEAR!"
+    break
 end
 end 
+end
 end
 
 def countdown_with_sleep
